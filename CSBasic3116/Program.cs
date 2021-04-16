64fad6f562853bd2cb947412d243893fe7a3fea4
seoyeong <josyeong1011@gmail.com>
2021-04-16 09:36:11 +0900

    초기 커밋.

---
+++ CSBasic3116/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSBasic3116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
