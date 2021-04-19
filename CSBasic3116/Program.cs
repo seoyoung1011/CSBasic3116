80c7cfe27b84276fcc828c95c05aa9820199716a
seoyeong <josyeong1011@gmail.com>
2021-04-19 14:42:55 +0900

    출력 예제 close #2

--- CSBasic3116/Program.cs
+++ CSBasic3116/Program.cs
@@ -7,13 +7,14 @@ namespace CSBasic3116
         static void Main(string[] args)
         {
             var name = "조" + "서" + "영";
-            Console.Write("개행이 안됨");
-            Console.WriteLine("개행이 됨");
 
             var alpah = "알파";
             var 베타 = "한글 변수가 되네";
-            Console.Write(alpah);
-            Console.WriteLine(베타);
+
+            //출력
+            Console.Write("개행이 안됨");
+            Console.WriteLine("");
+            Console.WriteLine("개행이 됨");
         }
     }
 }
