44b943c4753768ea7608eb7c300222a610cf848a
s2019s35 <s2019s35@e-mirim.hs.kr>
2021-05-03 14:57:15 +0900

    #16 반복문과 배열

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -6,7 +6,21 @@ namespace CSBasic3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* 반복문과 배열 */
+            int[] intArray = { 19, 27, 28, 26, 24 };
+            intArray[2] = 0;
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[1]);
+            Console.WriteLine(intArray[2]);
+            Console.WriteLine(intArray[3]);
+            Console.WriteLine(intArray[4]);
+            Console.WriteLine(intArray.Length + "\n\n");
+
+            int[] intArray2 = new int[100];
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[99]);
+
+
         }
     }
 }
