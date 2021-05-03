2df31367dfe4709456799481fe6898d3fefb0d82
s2019s35 <s2019s35@e-mirim.hs.kr>
2021-05-03 15:29:54 +0900

    #17 while / do while

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -17,8 +17,40 @@ namespace CSBasic3
             Console.WriteLine(intArray.Length + "\n\n");
 
             int[] intArray2 = new int[100];
-            Console.WriteLine(intArray[0]);
-            Console.WriteLine(intArray[99]);
+            Console.WriteLine(intArray2[0]);
+            Console.WriteLine(intArray2[99]);
+
+
+            /* while / do while 반복문 */
+            int i = 0;
+            int[] intArray3 = { 19, 27, 28, 26, 24 };
+            while(i < intArray3.Length)
+            {
+                Console.WriteLine((i+1) + "번째 출력: " + intArray3[i]);
+                i++;
+            }
+
+            string input;
+            do
+            {
+                Console.Write("exit를 입력하면 종료 : ");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("=> 공지사항");
+                        break;
+                    case "2":
+                        Console.WriteLine("=> 오늘의 점심");
+                        break;
+                    case "input":
+                        Console.WriteLine("do while문을 종료합니다.");
+                        break;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다.");
+                        break;
+                }
+            } while (input == "exit");
 
 
         }
