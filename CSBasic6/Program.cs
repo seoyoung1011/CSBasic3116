8c6f2301f37bf8b0f4d164cb2fc7dccf268fde42
s2019s35 <s2019s35@e-mirim.hs.kr>
2021-08-29 22:42:43 +0900

    #32 상속과 다형성

--- CSBasic6/Program.cs
+++ CSBasic6/Program.cs
@@ -39,7 +39,7 @@ namespace CSBasic6
 
         public Dog() { this.Age = 0; }
 
-        public void Eat() { Console.Write("냠냠 먹습니다."); }
+        public void Eat() { Console.WriteLine("냠냠 먹습니다."); }
 
         public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
         public void Bark() { Console.WriteLine("왈왈 짖습니다."); }
@@ -50,7 +50,7 @@ namespace CSBasic6
 
         public Cat() { this.Age = 0; }
 
-        public void Eat() { Console.Write("냠냠 먹습니다."); }
+        public void Eat() { Console.WriteLine("냠냠 먹습니다."); }
         public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
         public void Meow() { Console.WriteLine("냥냥 웁니다."); }
     }
