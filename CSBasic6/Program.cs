9d031c5f9703b42f12564c52a6abf7ccf8c8195c
s2019s35 <s2019s35@e-mirim.hs.kr>
2021-08-29 22:38:48 +0900

    초기 커밋.

---
+++ CSBasic6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSBasic6
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+
+            foreach(var item in Dogs)
+            {
+                item.Eat();
+
+                item.Sleep();
+                item.Bark();
+            }
+            foreach (var item in Cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
+        }
+    }
+    class Dog
+    {
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public Dog() { this.Age = 0; }
+
+        public void Eat() { Console.Write("냠냠 먹습니다."); }
+
+        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
+        public void Bark() { Console.WriteLine("왈왈 짖습니다."); }
+    }
+    class Cat
+    {
+        public int Age { get; set; }
+
+        public Cat() { this.Age = 0; }
+
+        public void Eat() { Console.Write("냠냠 먹습니다."); }
+        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
+        public void Meow() { Console.WriteLine("냥냥 웁니다."); }
+    }
+}
